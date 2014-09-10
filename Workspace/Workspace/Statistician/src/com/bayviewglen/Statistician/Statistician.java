0946bc462b12045a7c651a8f751dd8e71818d4ae
Aaron Strahle <astrahle@bayviewglen.ca>
2014-09-10 15:01:12 -0400

    this is a test

--- Workspace/Workspace/Statistician/src/com/bayviewglen/Statistician/Statistician.java
+++ Workspace/Workspace/Statistician/src/com/bayviewglen/Statistician/Statistician.java
@@ -1,10 +1,5 @@
 package com.bayviewglen.Statistician;
 
-public class Statistician {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Statistician { 
+// hi graeme
 }
